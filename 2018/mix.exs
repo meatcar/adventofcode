d3dd132c0b77706a0d62d6a1619ac656bf9075ba
Denys Pavlov <me@denys.me>
2018-12-03 04:49:04 +0800

    dialyzer
    jeez it's heavy

--- 2018/mix.exs
+++ 2018/mix.exs
@@ -7,7 +7,8 @@ defmodule Advent.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_deps: :transitive, plt_add_apps: [:mix]]
     ]
   end
 
@@ -24,7 +25,8 @@ defmodule Advent.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:httpoison, "~> 1.4"},
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
