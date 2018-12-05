4f093fb0dc6e10b5555daeadfd323eba1c5732ce
Denys Pavlov <me@denys.me>
2018-12-05 17:18:24 +0800

    templates

---
+++ 2018/lib/days/dayXX.ex
@@ -0,0 +1,33 @@
+defmodule DayXX do
+  @moduledoc """
+  Advent of Code Day XX
+  """
+
+  def cleanInput(input) do
+    input
+  end
+
+  @doc ~S"""
+
+  ### Example
+
+      iex> DayXX.part1("")
+      :not_implemented
+
+  """
+  def part1(input) do
+    :not_implemented
+  end
+
+  @doc ~S"""
+
+  ### Example
+
+      iex> DayXX.part2("")
+      :not_implemented
+
+  """
+  def part2(input) do
+    :not_implemented
+  end
+end
---
+++ 2018/test/days/dayXX_test.exs
@@ -0,0 +1,4 @@
+defmodule DayXXTest do
+  use ExUnit.Case, async: true
+  doctest DayXX
+end
