6fa82685deef317a8fd495b902a8aa0a36886528
Denys Pavlov <me@denys.me>
2020-12-02 22:14:47 -0500

    2020: day 2

--- 2020/days/common.zig
+++ 2020/days/common.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn readFile(allocator: *std.mem.Allocator, path: []const u8) ![]const u8 {
     const max_bytes = 10 * 1024 * 1024;
-    var file = try std.fs.cwd().readFileAlloc(allocator, path, max_bytes);
+    var file = try std.fs.cwd().readFileAlloc(allocator, path, std.math.maxInt(usize));
     return file;
 }
 
