243e4cc93f5e7ff3042fe616e688834e44e4b5d8
Denys Pavlov <me@denys.me>
2020-12-04 23:06:02 -0500

    2020: clean up readFile

--- 2020/days/common.zig
+++ 2020/days/common.zig
@@ -7,8 +7,7 @@ pub const parseInt = std.fmt.parseInt;
 pub const alloc = std.heap.page_allocator;
 
 pub fn readFile(allocator: *std.mem.Allocator, path: []const u8) ![]const u8 {
-    const max_bytes = 10 * 1024 * 1024;
-    var file = try std.fs.cwd().readFileAlloc(allocator, path, std.math.maxInt(usize));
+    const file = try std.fs.cwd().readFileAlloc(allocator, path, std.math.maxInt(usize));
     return file;
 }
 
