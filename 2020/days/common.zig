46eb0b333adb8b0de8e84ff4f60e6d77d1197078
Denys Pavlov <me@denys.me>
2020-12-04 22:53:53 -0500

    2020: reduce common boilerplate

--- 2020/days/common.zig
+++ 2020/days/common.zig
@@ -1,4 +1,10 @@
-const std = @import("std");
+pub const std = @import("std");
+pub const common = @import("./common.zig");
+pub const testing = std.testing;
+pub const mem = std.mem;
+pub const warn = std.debug.warn;
+pub const parseInt = std.fmt.parseInt;
+pub const alloc = std.heap.page_allocator;
 
 pub fn readFile(allocator: *std.mem.Allocator, path: []const u8) ![]const u8 {
     const max_bytes = 10 * 1024 * 1024;
